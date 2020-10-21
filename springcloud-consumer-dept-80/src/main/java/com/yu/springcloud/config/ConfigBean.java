f26954e07543cbcd98d10ec809d9469c50b816d1
caoyu <c375092047@163.com>
2020-10-21 18:03:03 +0800

    rabbon

--- springcloud-consumer-dept-80/src/main/java/com/yu/springcloud/config/ConfigBean.java
+++ springcloud-consumer-dept-80/src/main/java/com/yu/springcloud/config/ConfigBean.java
@@ -1,5 +1,6 @@
 package com.yu.springcloud.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -7,7 +8,10 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class ConfigBean {
 
+
     @Bean
+    //配置负载均衡实现 rabbon 客户端负载均衡
+    @LoadBalanced
     public RestTemplate getRestTemplate(){
         return new RestTemplate();
     }
---
+++ springcloud-provider-dept-8003/src/main/java/com/yu/springcloud/DeptProvider_8003.java
@@ -0,0 +1,17 @@
+package com.yu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+//启动类
+@SpringBootApplication
+@EnableEurekaClient//在服务启动后自动注册到Eureka中！
+@EnableDiscoveryClient//服务发现 用于在DeptController获取服务信息 discovery()
+public class DeptProvider_8003 {
+    public static void main(String[] args) {
+        SpringApplication.run(DeptProvider_8003.class,args);
+    }
+
+}
